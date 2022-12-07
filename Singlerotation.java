ee373ac0abb2444ea25a595b8d4892b31154ad09
priyajeeva1 <120002930+priyajeeva1@users.noreply.github.com>
2022-12-07 12:17:13 +0530

    Add files via upload

---
+++ Singlerotation.java
@@ -0,0 +1,13 @@
+public class Singlerotation
+{
+public static void main(String[] args)
+{
+int a[]={7,3,1,4,9};
+System.out.println(""+a[1]);
+for(int i=2;i<a.length;i++)
+{
+System.out.println(""+a[i]);
+}
+System.out.println(""+a[0]);
+}
+}
